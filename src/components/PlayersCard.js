3737eebf33a9a6fc7930140ff6de36aaaf6deb6f
@mudassir2 <mudassir@nimapinfotech.com>
2023-02-26 04:00:03 +0530

    ui issues done

--- src/components/PlayersCard.js
+++ src/components/PlayersCard.js
@@ -7,7 +7,7 @@ function PlayersCard({ player }) {
         <div className="card mt-3">
           <img
             className="card-img-top"
-            src={`../player-images/${player.Id}.jpg`}
+            src={`../../player-images/${player.Id}.jpg`}
             onError={(e) => {
               e.target.src =
                 "https://via.placeholder.com/500x500?text=Football";
@@ -15,18 +15,24 @@ function PlayersCard({ player }) {
             alt={`Player ${player.Id}`}
           />
           <div className="card-body">
-            <h5 className="card-title">{player.PFName}</h5>
+            <div className="d-flex justify-content-between">
+              <h5 className="card-title">{player.PFName}</h5>
+              <p className="text-success text-underline">${player.Value}</p>
+            </div>
             <p className="card-text">{player.SkillDesc}</p>
-            <p className="text-success text-underline">${player.Value}</p>
+
             {player?.UpComingMatchesList?.map((up) => {
               return (
                 <>
                   {up.CCode && up.VsCCode && (
-                    <p>
-                      {up.CCode ? up.CCode : ""}
-                      <span className="m-2">vs</span>
-                      {up.VsCCode ? up.VsCCode : ""}
-                    </p>
+                    <>
+                      <p className="fs-6 fw-bold">Upcoming Matches :</p>
+                      <p>
+                        {up.CCode ? up.CCode : ""}
+                        <span className="m-2">vs</span>
+                        {up.VsCCode ? up.VsCCode : ""}
+                      </p>
+                    </>
                   )}
                   <p>
                     <small>{up.MDate.toLocaleString()}</small>
