3b7ada4064113de696a829bd9aea725fe9d4b5de
@mudassir2 <mudassir@nimapinfotech.com>
2023-02-25 21:51:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
                 <h5 className="card-title">Name 1</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
-                  the bulk of the card's content.s
+                  the bulk of the card's content.
                 </p>
               </div>
             </div>
